cff0a5beeb89ab7323d4f1f19a8f79faf3d3a28f
Michal Lukac <michal.lukac@visma.com>
2022-09-08 14:39:21 +0200

    Initial project commit

---
+++ source/Web/Program.cs
@@ -0,0 +1,33 @@
+using Architecture.Application;
+using Architecture.Database;
+using DotNetCore.AspNetCore;
+using DotNetCore.IoC;
+using DotNetCore.Logging;
+using DotNetCore.Security;
+
+var builder = WebApplication.CreateBuilder();
+
+builder.Host.UseSerilog();
+
+builder.Services.AddResponseCompression();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSpaStaticFiles("Frontend");
+builder.Services.AddAuthenticationJwtBearer(new JwtSettings(Guid.NewGuid().ToString(), TimeSpan.FromHours(12)));
+builder.Services.AddContext<Context>(options => options.UseSqlServer(builder.Services.GetConnectionString(nameof(Context))));
+builder.Services.AddClassesMatchingInterfaces(typeof(UserService).Assembly, typeof(UserRepository).Assembly);
+builder.Services.AddControllers().AddJsonOptions().AddAuthorizationPolicy();
+
+var application = builder.Build();
+
+application.UseException();
+application.UseHttps();
+application.UseResponseCompression();
+application.UseSwagger();
+application.UseSwaggerUI();
+application.UseRouting();
+application.UseAuthentication();
+application.UseAuthorization();
+application.UseEndpoints(endpoints => endpoints.MapControllers());
+application.UseSpaAngular("Frontend", "start", "http://localhost:4200");
+
+application.Run();
